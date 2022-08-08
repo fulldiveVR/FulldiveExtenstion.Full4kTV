2238137b8f07158591c9b300024e4a3a6528864b
Noman Rehan <nomanrehan54@gmail.com>
2022-08-09 01:01:38 +0500

    Full-4k-TV

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
-# IPTV-Android
+# Full 4k TV
 
-Android application for live streaming. Source is taken from <a href="https://github.com/iptv-org/iptv" target="_BLANK">https://github.com/iptv-org/iptv</a> using jsoup and m3u parse in realtime.
+[comment]: <> (Android application for live streaming. Source is taken from <a href="https://github.com/iptv-org/iptv" target="_BLANK">https://github.com/iptv-org/iptv</a> using jsoup and m3u parse in realtime.)
 
-# Screenshots
-<p align='center'>
-    <img src="https://github.com/bachors/IPTV-Android/blob/master/captures/1.jpg" height="450px"/>
-    <img src="https://github.com/bachors/IPTV-Android/blob/master/captures/2.jpg" height="450px"/>
-    <img src="https://github.com/bachors/IPTV-Android/blob/master/captures/3.jpg" width="450px"/>
-</p>
+[comment]: <> (# Screenshots)
+
+[comment]: <> (<p align='center'>)
+
+[comment]: <> (    <img src="https://github.com/bachors/IPTV-Android/blob/master/captures/1.jpg" height="450px"/>)
+
+[comment]: <> (    <img src="https://github.com/bachors/IPTV-Android/blob/master/captures/2.jpg" height="450px"/>)
+
+[comment]: <> (    <img src="https://github.com/bachors/IPTV-Android/blob/master/captures/3.jpg" width="450px"/>)
+
+[comment]: <> (</p>)
 
 
-<a href="https://github.com/bachors/IPTV-Android/raw/master/app-debug.apk"><h2>DEMO APK</h2></a>
+[comment]: <> (<a href="https://github.com/bachors/IPTV-Android/raw/master/app-debug.apk"><h2>DEMO APK</h2></a>)
---
+++ app/src/main/java/com/full/k/tv/private1/channels/MainActivity.java
@@ -0,0 +1,131 @@
+package com.full.k.tv.private1.channels;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.full.k.tv.private1.channels.util.SharedPrefManager;
+import com.fulldive.startapppopups.PopupManager;
+import com.fulldive.startapppopups.donation.DonationAction;
+import com.fulldive.startapppopups.donation.DonationManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
+public class MainActivity extends AppCompatActivity implements Function1<DonationAction, Unit> {
+
+    SharedPrefManager sharedPrefManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPrefManager = new SharedPrefManager(this);
+        if(sharedPrefManager.getSpPlaylist().equals("") || sharedPrefManager.getSpPlaylist().equals("[]")) {
+            loadMain();
+        }else{
+            goTo();
+        }
+    }
+    @Override
+    public void onStart() {
+        new PopupManager().onAppStarted(this,"com.full.k.tv.private1.channels",true,true,true,550,this);
+        super.onStart();
+    }
+    @Override
+    protected void onDestroy() {
+        DonationManager s = DonationManager.INSTANCE;
+        s.destroy();
+        super.onDestroy();
+    }
+    private void loadMain() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final JSONArray json = new JSONArray();
+
+                try {
+                    Document doc = Jsoup.connect("https://raw.githubusercontent.com/FDweb0/Full-4k-TV/master/README.md").get();
+
+                    Elements tables = doc.select("table");
+
+                    for (Element table : tables) {
+                        JSONArray ar = new JSONArray();
+                        Elements trs = table.select("tr");
+                        int i = 0;
+                        for (Element tr : trs) {
+                            if(i > 0) {
+                                JSONObject ob = new JSONObject();
+                                ob.put("title", tr.select("td").eq(0).text());
+                                ob.put("link", tr.select("td").eq(2).select("code").eq(0).text());
+                                ar.put(ob);
+                            }
+                            i++;
+                        }
+                        json.put( ar);
+                    }
+                } catch (IOException ignored) {
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        sharedPrefManager.saveSPString(SharedPrefManager.SP_PLAYLIST, json.toString());
+                        goTo();
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void goTo(){
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        Intent launchNextActivity;
+                        launchNextActivity = new Intent(getApplicationContext(), PlaylistActivity.class);
+                        launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        launchNextActivity.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(launchNextActivity);
+                        overridePendingTransition(0, 0);
+                    }
+                },
+                5000);
+    }
+
+    @Override
+    public Unit invoke(DonationAction donationAction) {
+        if (String.valueOf(donationAction).contains("OpenedFromPopup")){
+            //Toast.makeText(this,"open",Toast.LENGTH_LONG).show();
+            DonationManager.INSTANCE.purchase(this);
+
+            //new PopupManager().showDonationSuccess(MainActivity.this);
+
+        }
+        Log.e("", String.valueOf(donationAction));
+        //Toast.makeText(this,donationAction.toString(),Toast.LENGTH_LONG).show();
+        return null;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
