5c3a46b3a482bf299b085c6d3781e30751c2f12a
FDweb0 <110075941+FDweb0@users.noreply.github.com>
2022-09-05 05:33:29 +0500

    Icon Fixed , Support Us Text Fixed
    -> Icon Fixed ,
    -> Support Us Text Fixed
    -> Package name changed

---
+++ app/build/generated/source/buildConfig/debug/com/full/k/tv/top/secure/channels/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.full.k.tv.top.secure.channels;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.full.k.tv.top.secure.channels";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
